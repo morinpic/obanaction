fe02581a4ec944964452c087370eff886304a907
morinpic <morinpic0518@gmail.com>
2015-06-23 18:29:11 +0900

    edit ホームページ修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @events = Event.where('start_time > ?', Time.zone.now).order(:start_time)
   end
 end
